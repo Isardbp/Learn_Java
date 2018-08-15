85257c25dbd9943a53559c264ea5db0278fd2bb0
root <root@localhost.localdomain>
2018-08-15 17:02:35 +0200

    arbol bin

---
+++ arbol_bin/Nodo.java
@@ -0,0 +1,40 @@
+public class Nodo{
+	//Declarar variables
+	private int valor;
+
+	private Nodo padre;
+	private Nodo hojaIzquierda;
+	private Nodo hojaDerecha;
+
+	//Constructor
+	public Nodo (int valor){
+		this.valor = valor;
+	}
+
+	//Setters and Getters 
+	public void setValor(int valor){
+		this.valor = valor;
+	}
+	public int getValor(){
+		return valor;
+	}
+	public Nodo getPadre(){
+		return padre;
+	}
+	public void setPadre(Nodo padre) {
+		 this.padre = padre;
+       	}
+	 
+	public Nodo getHojaIzquierda() {
+		 return hojaIzquierda;
+	}     
+	public void setHojaIzquierda(Nodo hojaIzquierda) {
+		this.hojaIzquierda = hojaIzquierda;
+	} 
+	public Nodo getHojaDerecha() {
+		return hojaDerecha;
+	}	     
+	public void setHojaDerecha(Nodo hojaDerecha) {
+		this.hojaDerecha = hojaDerecha;
+	}
+}
